d724727d8b623e01e9288bbe22043e0df832ede3
jsolosky735 <jjs7331@psu.edu>
2020-09-05 02:59:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Author: Jamys Solosky jjs7331@psu.edu  
+temp = input("Enter your course 1 letter grade: ")
+temp = input("Enter your course 1 credit: ")
+temp = input("Enter your course 2 letter grade: ")
+temp = input("Enter your course 2 credit: ")
+temp = input("Enter your course 3 letter grade: ")
+temp = input("Enter your course 3 credit: ")
